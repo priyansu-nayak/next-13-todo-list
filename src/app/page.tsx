9ab2566b0f555cda613585b9b8fcef0ba8132a49
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-08 13:04:22 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home(){
-  return <h1>Hi</h1>
+import Link from 'next/link'
+
+
+export default function Home() {
+  return <>
+    <header className='flex justify-between mb-4 items-center' >
+      <h1 className="text-2xl">Todos</h1>
+      <Link className='border border-slate-300 
+      text-slate-300 px-2 py-1 rounded 
+      hover:bg-slate-700 focus-within:bg-slate-700
+      outline-none ' href="/new">New</Link>
+    </header>
+    <ul>
+
+    </ul>
+  </>
 }
