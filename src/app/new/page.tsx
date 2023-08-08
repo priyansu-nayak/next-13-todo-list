096948bb9a8862a05a4dc4beb20aa1e215a3d9f5
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-08 17:42:05 +0530

    updated

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,12 @@
-export default function Page(){
-    return <h1>New</h1>
+export default function Page() {
+    return (<>
+        <header className='flex justify-between mb-4 items-center' >
+            <h1 className="text-2xl">New</h1>
+        </header>
+        <form action="">
+            <input type="text" name="title" 
+            className="border border-slate-300 bg-transparent 
+            rounded px-2 py-1 outline-none focus-within:border-slate-100" />
+        </form>
+    </>)
 }
