1786bd7d1ff1661e7786e38a30829854e93a5775
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-08 20:17:58 +0530

    updated

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,12 +1,25 @@
+import Link from "next/link";
+
 export default function Page() {
     return (<>
         <header className='flex justify-between mb-4 items-center' >
             <h1 className="text-2xl">New</h1>
         </header>
-        <form action="">
-            <input type="text" name="title" 
-            className="border border-slate-300 bg-transparent 
+        <form className="flex gap-2 flex-col">
+            <input type="text" name="title"
+                className="border border-slate-300 bg-transparent 
             rounded px-2 py-1 outline-none focus-within:border-slate-100" />
+            <div className="">
+                <Link href="" className="border border-slate-300 
+                text-slate-300 px-2 py-1 rounded 
+                hover:bg-slate-700 focus-within:bg-slate-700
+                outline-none">Cancel</Link>
+                <button className="border border-slate-300 
+            text-slate-300 px-2 py-1 rounded 
+            hover:bg-slate-700 focus-within:bg-slate-700
+            outline-none">Create</button>
+            </div>
+
         </form>
     </>)
 }
