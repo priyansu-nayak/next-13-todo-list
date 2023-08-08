6b4b34c92f4443e2bd5a65b5c9c1260354799431
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-08 20:24:23 +0530

    updated

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,15 +1,22 @@
 import Link from "next/link";
 
+async function createTodo (data:FormData){
+    // "use server"
+    console.log("HI")
+}
+
 export default function Page() {
     return (<>
         <header className='flex justify-between mb-4 items-center' >
             <h1 className="text-2xl">New</h1>
         </header>
+
         <form className="flex gap-2 flex-col">
             <input type="text" name="title"
                 className="border border-slate-300 bg-transparent 
             rounded px-2 py-1 outline-none focus-within:border-slate-100" />
-            <div className="">
+            
+            <div className="flex gap-2 justify-end">
                 <Link href="" className="border border-slate-300 
                 text-slate-300 px-2 py-1 rounded 
                 hover:bg-slate-700 focus-within:bg-slate-700
