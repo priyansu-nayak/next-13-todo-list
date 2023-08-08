8194f0891b24f7fb44d11750d5ab14f8e5a64aba
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-08 13:10:29 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { prisma } from '@/db'
 import Link from 'next/link'
 
 
-export default function Home() {
+export default async function Home() {
+
+  const todos = await prisma.todo.findMany(); //????
+
   return <>
     <header className='flex justify-between mb-4 items-center' >
       <h1 className="text-2xl">Todos</h1>
@@ -10,8 +14,12 @@ export default function Home() {
       hover:bg-slate-700 focus-within:bg-slate-700
       outline-none ' href="/new">New</Link>
     </header>
-    <ul>
 
+    <ul className='pl-4' >
+      {todos.map(todo =>(
+        <li key >{todo.title}</li>
+      ))}
     </ul>
+
   </>
 }
